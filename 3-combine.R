0b684458e0699c870502ecb1a837af25c1f20f60
robinlee93 <robincrlee@gmail.com>
2015-12-20 15:27:55 -0500

    more models and some recoding on weather data

--- 3-combine.R
+++ 3-combine.R
@@ -4,8 +4,14 @@ library(ggplot2)
 
 load("summary2014.RData")
 weather <- read_csv("2014weather.csv")
+weather$PrecipitationIn <- as.numeric(weather$PrecipitationIn)
+diff = tail(weather$`Mean TemperatureF`, -1) - head(weather$`Mean TemperatureF`, -1)
+diff = c(2,diff)
+weather$tempdiff = diff
+
 colnames(weather)[1] <- "date"
 summary2014$date <- as.Date(summary2014$date, format="%Y%m%d")
 comb <- inner_join(summary2014, weather)
 
 save(comb, file="combined2014.RData")
+save(summary2014, file="summary2014.RData")
--- 4-analyze.R
+++ 4-analyze.R
@@ -1,8 +1,64 @@
 library(ggplot2)
 library(dplyr)
 load("combined2014.RData")
-subscribers <- comb %>% filter(usertype=="Subscriber")
+load("summary2014.RData")
 
-ggplot(data=comb, aes(`Mean TemperatureF`, trips))+
+total <- summary2014 %>% group_by(date) %>% summarise(trips=sum(trips), totaltime=sum(totaltime))
+comb_total <- inner_join(total, weather)
+
+ggplot(data=comb_total, aes(`Mean TemperatureF`, trips))+
   geom_point()+
-  scale_y_continuous(limits=c(0,36000), expand=c(0,0))
+  scale_y_continuous(limits=c(0,40000), expand=c(0,0))+
+  ggtitle("Number of trips per day in 2014")
+
+m1 <- lm(data=comb_total, trips~`Mean TemperatureF`)
+
+d <- ggplot(data=comb, aes(`Mean TemperatureF`, trips))+
+  geom_point(alpha=.5)+
+  scale_y_continuous(expand = c(0,5)) 
+
+d + facet_grid(.~usertype, scales="free_y")
+d + facet_grid(usertype ~ . , scales = "free_y")
+
+m2 <-lm(data=comb, trips~`Mean TemperatureF`*usertype)
+summary(m2)
+# change in temp
+
+m3 <-lm(data=comb, trips~`Mean TemperatureF`*usertype+tempdiff)
+summary(m3)
+
+# preciptation
+
+m4 <- lm(data=comb, trips~`Mean TemperatureF`*usertype+PrecipitationIn)
+summary(m4)
+
+#recode rain as a dummy 
+comb$precipitat <- comb$PrecipitationIn!=0
+m5 <- lm(data=comb, trips~`Mean TemperatureF`*usertype+precipitat)
+summary(m5)
+# works better
+
+# weekday
+comb$weekday <- weekdays(comb$date)
+comb$weekend <- comb$weekday=="Saturday"|comb$weekday=="Sunday"
+
+m6 <- lm(data=comb, trips~`Mean TemperatureF`*usertype+precipitat+weekend)
+summary(m6)
+
+m7 <- lm(data=comb, trips~`Mean TemperatureF`*usertype+precipitat+weekend+weekend:usertype)
+summary(m7)
+## tempquadratic?
+
+ggplot(data=comb_total, aes(`Mean TemperatureF`, trips))+
+  geom_point()+
+  scale_y_continuous(limits=c(0,40000), expand=c(0,0))+
+  ggtitle("Number of trips per day in 2014")
+
+m8<- glm(data=comb_total, trips~poly(`Mean TemperatureF`,2))
+
+ggplot(data=comb_total, aes(`Max TemperatureF`, trips))+
+  geom_point()
++
+  scale_y_continuous(limits=c(0,40000), expand=c(0,0))+
+  ggtitle("Number of trips per day in 2014")
+
